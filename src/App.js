14089da4c848b997e629187b2fb528dbb4b4ba5f
iikim <iikim2511@gmail.com>
2021-02-11 13:34:30 -0500

    <header className="header">

--- src/App.js
+++ src/App.js
@@ -6,10 +6,22 @@ import './style.scss';
 function App() {
   return (
     <div className="App">
-    <h1>hello</h1>
-  <div></div>
+      <header className="header">
+        <h1>COVID-19</h1>
+          <select name="" id="">
+            <option value="">국내</option>
+            <option value="">세계</option>
+          </select>
+      
+      </header>
+
+      <section>
+        <h2>국내 코로나 현황</h2>
+        <div className="contents">
+        </div>
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
