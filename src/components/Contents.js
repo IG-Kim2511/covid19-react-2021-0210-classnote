1280e2c6e0db13c18f4ce2316823c5f18ac3e431
iikim <iikim2511@gmail.com>
2021-02-11 18:29:17 -0500

    no message

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,14 +1,39 @@
-import React from 'react'
+
+/* js 014: 
+useState: data관리
+useEffect : patch API 사용 
+
+npm install  axios*/
+
+import {useState, useEffect} from 'react'
+
+import {Bar, Dougnut, Line} form 'react-chart'
+
+
+
 import PropTypes from 'prop-types'
 
+import axios from 'axios'
+
 const Contents = props => {
+
+    useEffect(()=>{
+
+        const fetchEvents = async ()=>{
+            const res = await axios.get("https://api.covid19api.com/summary ")        
+
+
+            console.log(res)
+        }
+        fetchEvents()
+    })
     return (
         <div>
-        <section>
-        <h2>국내 코로나 현황</h2>
-        <div className="contents">
-        </div>
-      </section>
+            <section>
+            <h2>국내 코로나 현황</h2>
+            <div className="contents">
+            </div>
+        </section>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
-                
+        <div>                
          <header className="header">
             <h1>COVID-19</h1>
             <select name="" id="">
